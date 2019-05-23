91c5d26dec138c5de537847dc22dc6a096afa4d2
Christian Stolz <hg8496@cstolz.de>
2019-05-23 22:28:46 +0200

    Changed structure of energy day

--- test/energyDay.test.ts
+++ test/energyDay.test.ts
@@ -6,16 +6,17 @@ import ValueStream = janitza.values.ValueStream;
 
 const payloadEnergyPartial: IEnergyDay = {
     day: new Date().toISOString().substr(0, 10),
-    input: {channel: 1} as IInput,
 };
 
 test("Verifiable day set", () => {
     const day = EnergyDay.create(payloadEnergyPartial);
-    day.activeEnergy = ValueStream.create({timebase: janitza.values.TimeBase.Minutes15});
+    day.values = [];
+    const vs = ValueStream.create({timebase: janitza.values.TimeBase.Minutes15, input: {channel: 1} as IInput, type: janitza.values.EnergyValueType.ActiveEnergyConsumed});
+    day.values.push(vs);
     for(let i = 0; i < 86; ++i) {
-        day.activeEnergy.values[i] = i;
+        vs.values[i] = i;
     }
     const data = EnergyDay.encodeDelimited(day).finish();
-    expect(data.length).toBe(714);
+    expect(data.length).toBe(716);
     expect(EnergyDay.verify(day)).toBe(null);
 });
